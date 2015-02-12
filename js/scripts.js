e59c9f225453ba5f529b3724cb87bd1373857e0a
Heather Anderson & Sarah Joy Calpo <student@epicodus.com>
2015-02-12 11:31:23 -0800

    Rework specs to actually work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 var wordOrder = function(text) {
   text = text.toLowerCase();
   var splitText = text.split(" ");
-  var wordCount = []
-  var wordList = []
+  var word = []
+  var count =[]
   var prev;
 
   for (var i = 0; i < splitText.length; i++) {
     if (splitText[i] !== prev) {
-      wordCount.push(splitText[i]);
-      wordCount.push(1)
+      word.push(splitText[i]);
+      count.push(1)
     } else {
-      wordCount[1]++;
-  }
+      count[count.length-1]++;
+    }
     prev = splitText[i];
   }
-    wordList.push(wordCount);
 
-  return wordList;
-
+  return [word, count]
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,23 @@
 describe("wordOrder", function() {
 
   it("returns a word if it is found in a string", function() {
-    expect(wordOrder("hello")[0]).to.eql(["hello", 1]);
+    expect(wordOrder("hello")[0]).to.eql(["hello"]);
   });
 
   it("counts the number of times a word is in an array", function() {
-    expect(wordOrder("hello hello hello")).to.eql([["hello", 3]]);
+    expect(wordOrder("hello")).to.eql([["hello"], [1]]);
   });
+
+  it("counts the number of times a word is in an array", function() {
+    expect(wordOrder("hello world")).to.eql([["hello", "world"], [1, 1]])
+  });
+
+  it("counts the number of times a word is in an array", function() {
+    expect(wordOrder("hello hello world world world")).to.eql([["hello", "world"], [2, 3]])
+  });
+
+  it("lists the words in order of frequency", function() {
+    expect(wordOrder("hello goodbye goodbye")).to.eql([["goodbye", "hello"], [2,1]])
+  });
+
 });
