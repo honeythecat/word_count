6b48816c0f2231863fbe37e7fe29388ffb51995e
Heather Anderson & Sarah Joy Calpo <student@epicodus.com>
2015-02-12 11:07:18 -0800

    Add working spec and code for counting words.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 var wordOrder = function(text) {
   text = text.toLowerCase();
-  return text;
+  var splitText = text.split(" ");
+  var wordCount = []
+  var wordList = []
+  var prev;
+
+  for (var i = 0; i < splitText.length; i++) {
+    if (splitText[i] !== prev) {
+      wordCount.push(splitText[i]);
+      wordCount.push(1)
+    } else {
+      wordCount[1]++;
+  }
+    prev = splitText[i];
+  }
+    wordList.push(wordCount);
+
+  return wordList;
+
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe("wordOrder", function() {
-  it("lowercase all input", function() {
-    expect(wordOrder("GoodBye")).to.equal("goodbye");
+
+  it("returns a word if it is found in a string", function() {
+    expect(wordOrder("hello")[0]).to.eql(["hello", 1]);
+  });
+
+  it("counts the number of times a word is in an array", function() {
+    expect(wordOrder("hello hello hello")).to.eql([["hello", 3]]);
   });
 });
